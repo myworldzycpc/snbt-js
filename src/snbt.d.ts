3ad4bb80e48c8eabce6e76640674b0e08e30b878
NriotHrreion <nriot233@outlook.com>
2026-02-11 14:00:30 +0800

    Improve d.ts
    Added some generics

--- src/snbt.d.ts
+++ src/snbt.d.ts
@@ -1,9 +1,11 @@
 export declare function changeToFloat(val: number): number | string;
 export declare function highlightCode(text: string, type: "key" | "str" | "num" | "bool" | "unit" | string): string;
 
-export type NbtValue = NbtObject | NbtList | NbtIntArray | NbtNumber | NbtString | NbtBool | NbtNull;
+export declare abstract class NbtValue {
+  text(ispretty?: boolean): string;
+}
 
-export declare class NbtObject {
+export declare class NbtObject extends NbtValue {
   childs: Record<string, NbtValue>;
   constructor(childsal?: Record<string, NbtValue>);
   addChild(key: string, value: NbtValue): void;
@@ -12,57 +14,50 @@ export declare class NbtObject {
    * Get a child element by path.
    * @param path The path to the element (key or array of keys/indices)
    */
-  get(path: string | number | (string | number)[]): NbtValue | undefined;
-  get(...path: (string | number)[]): NbtValue | undefined;
+  get<V extends NbtValue = NbtValue>(path: string | number | (string | number)[]): V | undefined;
+  get<V extends NbtValue = NbtValue>(...path: (string | number)[]): V | undefined;
   set(index: string | number | (string | number)[], value: NbtValue): void;
-  text(ispretty?: boolean): string;
 }
 
-export declare class NbtList {
+export declare class NbtList extends NbtValue {
   childs: NbtValue[];
   constructor(childsal?: NbtValue[]);
   addChild(value: NbtValue): void;
   isempty(): boolean;
-  get(path: string | number | (string | number)[]): NbtValue | undefined;
-  get(...path: (string | number)[]): NbtValue | undefined;
+  get<V extends NbtValue = NbtValue>(path: string | number | (string | number)[]): V | undefined;
+  get<V extends NbtValue = NbtValue>(...path: (string | number)[]): V | undefined;
   set(index: string | number | (string | number)[], value: NbtValue): void;
-  text(ispretty?: boolean): string;
 }
 
-export declare class NbtIntArray {
+export declare class NbtIntArray extends NbtValue {
   childs: NbtNumber[];
   constructor(childsal?: NbtNumber[]);
   addChild(value: NbtNumber): void;
   isempty(): boolean;
-  get(path: string | number | (string | number)[]): NbtValue | undefined;
-  get(...path: (string | number)[]): NbtValue | undefined;
+  get<V extends NbtValue = NbtValue>(path: string | number | (string | number)[]): V | undefined;
+  get<V extends NbtValue = NbtValue>(...path: (string | number)[]): V | undefined;
   set(index: string | number | (string | number)[], value: NbtNumber): void;
-  text(ispretty?: boolean): string;
 }
 
-export declare class NbtNumber {
+export declare class NbtNumber extends NbtValue {
   value: number;
   unit: string;
   constructor(value: number, unit?: string);
-  text(ispretty?: boolean): string;
 }
 
-export declare class NbtString {
+export declare class NbtString extends NbtValue {
   value: string;
   constructor(value: string);
-  text(ispretty?: boolean): string;
 }
 
-export declare class NbtBool {
+export declare class NbtBool extends NbtValue {
   value: string;
   constructor(value: boolean);
-  text(ispretty?: boolean): string;
 }
 
-export declare class NbtNull {
+export declare class NbtNull extends NbtValue {
   value: null;
   constructor();
-  text(ispretty?: boolean): string;
 }
 
 export declare function arrangementNbt(str: string): string;
